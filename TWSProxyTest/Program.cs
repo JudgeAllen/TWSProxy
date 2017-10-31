a6ec239c1c56dfc14107951923d0a51f39458cf9
Xin Chen <chenxin@aliyun.com>
2017-10-31 22:04:56 +0800

    Add events and sample code

--- TWSProxyTest/Program.cs
+++ TWSProxyTest/Program.cs
@@ -9,17 +9,32 @@ namespace TWSProxyTest
 {
     class Program
     {
+        static void OnPrice(OnPriceMessage msg)
+        {
+            Console.WriteLine("Symbol: {0}, Type: {1}, Price: {2}, PreOpen: {3}", msg.Symbol, msg.Type, msg.Price, msg.PreOpen);
+        }
+
+        static void OnGreeks(OnGreeksMessage msg)
+        {
+            Console.WriteLine("Symbol: {0}, Type: {1}, Delta: {2}, Gamma: {3}, Vega: {4}, Theta: {5}, IV: {6}, OptPrice: {7}, PvDividend: {8}, UndPrice: {9}", 
+                msg.Symbol, msg.Type, msg.Delta, msg.Gamma, msg.Vega, msg.Theta, msg.ImpliedVolatility, msg.OptPrice, msg.PvDividend, msg.UndPrice);
+        }
+
         static void Main(string[] args)
         {
-            IBProxy proxy = new IBProxy(true);
+            IBProxy proxy = new IBProxy(false);
 
             proxy.connect("127.0.0.1", 7496, 1);
+
+            proxy.OnPriceEvent += OnPrice;
+            proxy.OnGreeksEvent += OnGreeks;
 
             proxy.add("SPY");
             proxy.add("STK.VXX");
             proxy.add("OPT.VXX.C.20171117.35.5");
             proxy.add("FUT.ES.20171215");
             proxy.add("FOP.ES.C.20171215.2550");
+
 
             Console.ReadKey();
         }
